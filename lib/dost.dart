fcc0dd44729e81c4cf744e65b519a15c9188ca6b
sascha <sascha.lutzmann@gmail.com>
2019-11-07 17:15:24 +0100

    Initial commit

---
+++ lib/dost.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dost;
+
+export 'src/dost_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
