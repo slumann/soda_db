614b20142b96be58ca9d5299daa7fffc5410cd2c
sascha <sascha.lutzmann@gmail.com>
2019-11-07 17:20:40 +0100

    Initial project setup

--- lib/dost.dart
+++ lib/dost.dart
@@ -1,8 +1,5 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library dost;
 
-export 'src/dost_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/storage.dart';
+export 'src/collection.dart';
+export 'src/document.dart';
