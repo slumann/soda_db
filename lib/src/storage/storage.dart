520c69c4cccaf5e691d6c6815afbb24e2ca412e7
sascha <sascha.lutzmann@gmail.com>
2019-12-15 21:26:18 +0100

    Added global storage variable so that storage can be used as singleton

--- lib/src/storage/storage.dart
+++ lib/src/storage/storage.dart
@@ -1,5 +1,14 @@
+import 'package:meta/meta.dart';
 import 'package:soda_db/src/storage/repository.dart';
 import 'package:soda_db/src/storage/soda_entity.dart';
+import 'package:soda_db/src/storage/storage_impl.dart';
+
+Storage _storage;
+
+Storage get storage => _storage ??= StorageImpl();
+
+@visibleForTesting
+void set storage(Storage value) => _storage = value;
 
 typedef EntityFactory<T> = T Function(Map<String, dynamic>);
 
